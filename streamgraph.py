0d5b94a176223cf846922b950aa55bd18cd6c22a
Chris Berry <cmb383@cornell.edu>
2013-11-19 12:37:44 -0500

    Add calibrated graphing.
     - Hard code a calibration made by inspection of sample data.
     - Improve graph labels.

--- streamgraph.py
+++ streamgraph.py
@@ -17,19 +17,26 @@ implement as:
 color sample for yellow ruler in image CDY_0450.JPG is
 #DE9D33, or hsv:37,197,222
 
-fixme:
+TODO:
 do edge detection one the contour with the largest height
-depth is the compliment of the length of ruler visible WRT max visible ruler :)
 make mask from clean image with max ruler showing...
+
+manual calibration by inspection of CDY_0450.JPG from sample set:
+	y = 2px = 5.28 ft
+	y = 1485px = 0.0 ft
+	1483px/63.36in = 23.4px/in
+	(1485px-1536px)/(23.4px/in) = -2.18in @ bottom of frame
 """
+
 import os
-import datetime as dt
+import datetime
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
+from matplotlib import pyplot as plt
+from matplotlib import dates
 from PIL import Image
 from PIL import ExifTags
+from operator import itemgetter
 
 def extractHoughLine(line):
     for rho,theta in line:
@@ -48,8 +55,10 @@ def showme(image, imagesize=(1024, 768)):
     cv2.namedWindow('Display Window') #flags=0 for resizeable window
     cv2.imshow('Display Window',resizedImage)
     cv2.waitKey(0)
+length_calibration = 23.4 # px/in
+offset_calibration = -51 # px elevation @ 0 inches
 times = []
-contour_height = []
+calibrated_depths = []
 tseries = []
 for file in os.listdir('./images'):
     try:
@@ -62,9 +71,9 @@ for file in os.listdir('./images'):
         }
         image = cv2.imread('./images/'+file, cv2.CV_LOAD_IMAGE_COLOR)
     except:
-        print "failed to read: "
-        print file
+        print "failed to read: "+ file
         continue
+
     hsv = cv2.cvtColor(image, cv2.cv.CV_BGR2HSV)
     cthresh  = cv2.inRange(hsv, (20, 100, 160), (35, 255, 255))
     drawing = np.zeros(cthresh.shape,np.uint8) # Image to draw the contours
@@ -78,10 +87,17 @@ for file in os.listdir('./images'):
     #     cv2.drawContours(drawing,[cnt],0,[255, 255, 255],-1)    
     contours = sorted(contours, key=cv2.contourArea)
     largest = contours[-1]
-    # height of largest contour
-    contour_height.append(int(max(largest[:,:,1])-min(largest[:,:,1])))
-#     t =  dt.datetime.strptime(exif['DateTimeOriginal'], '%Y:%m:%d %H:%M:%S')
-    times.append(dt.datetime.strptime(exif['DateTimeOriginal'],'%Y:%m:%d %H:%M:%S'))
+
+    # height of the bottom of the visible part of ruler from bottom of frame
+    # equals y size, in pixels, minus largest y co-ord (farthest from top)
+    # px      = image height          - lowest px in contour
+    height_px = int(np.shape(image)[0]-max(largest[:,:,1]))
+
+    # height in inches
+    calibrated_depths.append((height_px+offset_calibration)/length_calibration)
+    print file+' '+str(calibrated_depths[-1])[0:5]+' in.@'+exif['DateTimeOriginal']
+#     t =  datetime.datetime.strptime(exif['DateTimeOriginal'], '%Y:%m:%d %H:%M:%S')
+    times.append(datetime.datetime.strptime(exif['DateTimeOriginal'],'%Y:%m:%d %H:%M:%S'))
 #     tseries.append([depth, t])
     
     cv2.drawContours(colorimage,[contours[-1]],0,[255, 0, 255],-1)
@@ -93,7 +109,18 @@ for file in os.listdir('./images'):
 
     showme(colorimage)
 
+# sort by date 
+times, calibrated_depths = (list(x) for x in zip(*sorted(zip(times, calibrated_depths),key=itemgetter(0))))
+
 fig, ax = plt.subplots()
-ax.plot(times, contour_height, 'go')
+ax.plot(times, calibrated_depths, 'g-')
+hfmt = dates.DateFormatter('%m/%d %H:%M')
+ax.xaxis.set_major_formatter(hfmt)
+ax.set_ylim(bottom = 0)
+plt.subplots_adjust(bottom=.2)
+plt.xticks(rotation='vertical')
+fig.suptitle('Stream Stage', fontsize=14)
+plt.ylabel('Inches', fontsize=12)
 fig.autofmt_xdate()
 plt.show()
+fig.savefig('streamgraph_sample.jpg')
